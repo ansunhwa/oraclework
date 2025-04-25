95d56548e119f17ed8e748148a25d53e67ee7948
Ansunhwa <hbo1053@naver.com>
2025-04-25 18:28:19 +0900

    4.25

--- 사용자계정.sql
+++ 사용자계정.sql
@@ -2,3 +2,4 @@ ALTER SESSION SET "_oracle_script" = true;
 CREATE USER semi IDENTIFIED BY 1234;
 GRANT CONNECT, RESOURCE TO semi;
 ALTER USER semi default tablespace users quota unlimited on users;
+ALTER TABLE food_logs ADD MEAL_TIME VARCHAR2(20);
